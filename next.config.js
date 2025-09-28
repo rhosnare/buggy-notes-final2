d1fccf93abfa2f87d17e591219b448652a2c9d85
rhosnare <muhammadrhosid5@gmail.com>
2025-09-28 22:22:48 +0700

    coba ulang3

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+// Menggunakan 'import' standar ES Module (dari kode asli Anda)
+await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  // Flag ini penting untuk memperbaiki build di Netlify
+  optimizeFonts: false,
+};
 
+// Menggunakan 'export default' standar ES Module (dari kode asli Anda)
 export default config;
